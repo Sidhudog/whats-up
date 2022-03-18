4ced5e9cfc720deb347a98fef64a7f239585e358
Atharva Deshmukh <81254891+Atharva-Deshmukh@users.noreply.github.com>
2022-03-18 21:24:41 +0530

    Add files via upload

---
+++ table.js
@@ -0,0 +1,52 @@
+object = [
+    {
+      "threw": "frighten",
+      "me": "suit",
+      "noun": "sort",
+      "swept": "electricity",
+      "expect": "not",
+      "like": "she"
+    },
+    {
+      "threw": "browserling",
+      "me": "corner",
+      "noun": "bone",
+      "swept": "crack",
+      "expect": "thou",
+      "like": "appropriate"
+    },
+    {
+      "threw": "please",
+      "me": "drive",
+      "noun": "carried",
+      "swept": "cake",
+      "expect": "means",
+      "like": "welcome"
+    },
+    {
+      "threw": "putting",
+      "me": "bottom",
+      "noun": "cream",
+      "swept": "valuable",
+      "expect": "gone",
+      "like": "am"
+    },
+    {
+      "threw": "western",
+      "me": "those",
+      "noun": "burst",
+      "swept": "mind",
+      "expect": "third",
+      "like": "after"
+    },
+    {
+      "threw": "children",
+      "me": "although",
+      "noun": "friendly",
+      "swept": "pleasure",
+      "expect": "noun",
+      "like": "creature"
+    }
+  ]
+
+
